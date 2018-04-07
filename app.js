777a3f7993a13bad1e9f93dff888529f66f24b5a
shivam123425 <shivam123425@gmail.com>
2018-04-07 20:54:17 +0530

    Bug - Unix input => incorrect result

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req,res){
+    res.send("Enter Something after / in URL");
+})
+
+app.get("/:dateString", function(req,res){
+    // res.send(req.params.dateString);
+    let date = req.params.dateString;
+    if (! isNaN(date)) {
+        date = Number(date);
+    }
+    const possibleDate = new Date(date);
+    console.log(possibleDate);
+    if (possibleDate=="Invalid Date") {
+        return res.send("Invalid Date");
+    }
+    const dateObject = {
+        "unix": Math.round(possibleDate.getTime()),
+        "Date": possibleDate.toString()
+    }
+    res.send(JSON.stringify(dateObject));
+});
+
+app.listen(8000, function() {
+    console.log("Started Server at 8000");
+})
+
